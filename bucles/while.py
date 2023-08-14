f9f62a2df48c6efee4737f4b4b4a0accf4649812
Luis Puc <Luis.Puc@asd-tech.mx>
2023-08-13 20:18:57 -0600

    bucles

---
+++ bucles/while.py
@@ -0,0 +1,13 @@
+# Es un pedazo de código que se ejecuta repetidamente.
+# While: requiere una condición para ejecutar cierto código según tantas veces se cumpla dicha condición.
+# Do While: primero ejecuta cierto código y luego evalua una condición para determinar si se requiere ejecutar nuevamente.
+# For: Iterar bajo una condición finita.
+# Foreach: Itera una serie de datos (lo que haya en una lista de datos).
+
+numberPrint = 500
+i = 0
+while i < numberPrint:
+    print("TE AMO <3")
+    i=i+1
+
+print("TE AMARÉ POR SIEMPRE...")
