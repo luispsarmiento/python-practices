1035f8ed91e187529bfc58d2bf306c4213cbed9a
Luis Puc <j.louis1493@gmail.com>
2025-03-27 03:50:12 +0000

    Added: APIs with JSON demo

--- flask_api_demo/main.py
+++ flask_api_demo/main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/me")
+def me_api():
+    return {
+        "username": "Luis P. Sarmiento",
+        "theme": "dark",
+        ##"image": url_for("user_image", filename=user.image),
+    }
